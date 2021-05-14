68ab6fb819c90b7dd391d71b82625224c58250ad
tonicfilip <tonicfilip@outlook.com>
2021-05-14 17:26:49 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { getTodos } from "./services/index";
 
 function App() {
+  const [serviceData, setServiceData] = useState([]);
+
+  useEffect(() => {
+    getTodos().then((res) => setServiceData(res.data));
+    console.log(serviceData);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Service result</h1>
+      <hr />
+      <ul>
+        {serviceData.map((data) => (
+          <li key={data.id}>
+            title: {data.title},
+            <br />
+            userId: {data.userId}
+            <br />
+            completed: {data.completed ? "true" : "false"}
+            <hr />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
